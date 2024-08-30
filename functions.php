e2eff813cebf808978a15a651e19f927fb3cf765
Driton <106562685+DritonLatifi@users.noreply.github.com>
2024-08-30 20:07:23 +0200

    init

---
+++ functions.php
@@ -0,0 +1,9 @@
+<!-- adds stylesheet to html -->
+
+<?php 
+    function add_stylesheet_to_head() {
+        echo "<link href='".get_stylesheet_uri()."' rel='stylesheet' type='text/css'>";
+    }
+
+    add_action( 'wp_head', 'add_stylesheet_to_head' ); 
+?>
